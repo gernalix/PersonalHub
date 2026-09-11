f67e8927ddfd755a0d8970d1e431225bd4592d87
gernalix <galatidaniele@gmail.com>
2026-09-11 22:53:56 +0200

    Use PersonalHub version in Soldi

--- feature/soldi/build.gradle.kts
+++ feature/soldi/build.gradle.kts
@@ -2,12 +2,22 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.compose)
 }
+
+val personalHubVersion = rootProject.file("version.txt").readText().trim().toInt()
+
 android {
     namespace = "com.gernalix.personalhub.soldi"
     compileSdk = 37
-    defaultConfig { minSdk = 29 }
+    defaultConfig {
+        minSdk = 29
+        buildConfigField("int", "VERSION_CODE", personalHubVersion.toString())
+        buildConfigField("String", "VERSION_NAME", "\"$personalHubVersion\"")
+    }
     compileOptions { sourceCompatibility = JavaVersion.VERSION_11; targetCompatibility = JavaVersion.VERSION_11 }
-    buildFeatures { compose = true }
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
     testOptions { unitTests.isIncludeAndroidResources = true }
 }
 dependencies {
