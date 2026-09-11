06128e1696bdc86ef073529c866732cfe30ee309
gernalix <galatidaniele@gmail.com>
2026-09-11 22:42:54 +0200

    Make Timer theme follow system

--- feature/multitimetracker/src/main/java/com/example/multitimetracker/ui/theme/Theme.kt
+++ feature/multitimetracker/src/main/java/com/example/multitimetracker/ui/theme/Theme.kt
@@ -2,6 +2,7 @@
 package com.example.multitimetracker.ui.theme
 
 import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.darkColorScheme
@@ -44,7 +45,7 @@ private val DarkColors = darkColorScheme(
 
 @Composable
 fun MultiTimeTrackerTheme(
-    darkTheme: Boolean = false,
+    darkTheme: Boolean = isSystemInDarkTheme(),
     useDynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
