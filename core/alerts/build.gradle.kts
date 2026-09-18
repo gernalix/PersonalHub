e3f1568670b6a42da94907d8b3792fb14429ca97
gernalix <galatidaniele@gmail.com>
2026-09-18 19:39:02 +0200

    Add shared alerts foundation: build.gradle.kts

---
+++ core/alerts/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    alias(libs.plugins.android.library)
+}
+
+android {
+    namespace = "com.gernalix.personalhub.core.alerts"
+    compileSdk = 37
+    defaultConfig { minSdk = 29 }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(project(":contracts:database"))
+    implementation(project(":core:database"))
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.kotlinx.coroutines.android)
+}
