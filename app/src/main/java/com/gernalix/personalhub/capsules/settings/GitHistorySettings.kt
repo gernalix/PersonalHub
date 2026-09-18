d7dcdf859e8ab5572fa5bcce1da6386b50f6eae4
gernalix <galatidaniele@gmail.com>
2026-09-18 15:49:42 +0200

    Fix History screen layout import

--- app/src/main/java/com/gernalix/personalhub/capsules/settings/GitHistorySettings.kt
+++ app/src/main/java/com/gernalix/personalhub/capsules/settings/GitHistorySettings.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.AlertDialog
