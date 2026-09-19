0b6da840613315a9358ddb69d0512a3cb1a094db
gernalix <galatidaniele@gmail.com>
2026-09-19 12:18:40 +0200

    Delete standalone-era feature transfer implementations

--- feature/wordpulse/src/main/java/com/wordpulse/app/data/BackupImportResult.kt
+++
@@ -1,8 +0,0 @@
-package com.wordpulse.app.data
-
-data class BackupImportResult(
-    val sessionsImported: Int,
-    val wordsImported: Int,
-    val correctionsImported: Int = 0,
-    val pvtResultsImported: Int = 0,
-)
