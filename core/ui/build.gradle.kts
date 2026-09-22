484bfa6a973e255c0f7b55c3152985a778194495
gernalix <galatidaniele@gmail.com>
2026-09-22 17:44:55 +0200

    test: enable shared UI unit tests

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -19,4 +19,5 @@ dependencies {
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
+    testImplementation(libs.junit)
 }
