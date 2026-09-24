82fef4ca53ec9329164d9ceb304f3276ee01b2af
gernalix <galatidaniele@gmail.com>
2026-09-24 04:54:09 +0200

    Gate database-backed Activity on schema readiness

--- feature/sostanze/src/main/java/com/gernalix/sostanze/MainActivity.kt
+++ feature/sostanze/src/main/java/com/gernalix/sostanze/MainActivity.kt
@@ -2,6 +2,7 @@ package com.gernalix.sostanze
 
 import android.content.Intent
 import android.os.Bundle
+import com.gernalix.personalhub.core.database.DatabaseStartupGate
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -16,6 +17,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (DatabaseStartupGate.blockIfNotReady(this)) return
         hubSubstanceId = intent.hubSubstanceId()
         enableEdgeToEdge()
         setContent {
